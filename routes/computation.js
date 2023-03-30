62612ac0a8248388c3ba9366236eb8c9c0ab89f3
kyathijagadeeswarp <123043429+kyathijagadeeswarp@users.noreply.github.com>
2023-03-29 21:16:13 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -4,9 +4,9 @@ router.get('/', function(req, res, next) {
     var d1 = Math.floor(Math.random() * 10);
     var d2 = Math.floor(Math.random() * 02);
     var d3 = Math.floor(Math.random() * 10);
-    var cosFunction = Math.log2(d1);
-    var asinFunction = Math.cosh(d2);
-    var asinhFunction = Math.floor(d3);
+    var log2Function = Math.log2(d1);
+    var coshFunction = Math.cosh(d2);
+    var floorFunction = Math.floor(d3);
     res.render('computation',
                 {
                     title: 'Kyathi Jagadeeswar Pagadala functions for computations. ',
